acf5b521343cb9c85ac715d7e72eb0422ca3533a
Sean <sgroff04@gmail.com>
2018-01-22 07:46:53 -0600

    created a graphql schema and mock data

--- server/data/mocks.js
+++ server/data/mocks.js
@@ -1,3 +1,49 @@
+const casual = require('casual');
+const { MockList } = require('graphql-tools');
+
 module.exports = {
-  String: () => 'It works!',
+  String: () => casual.title,
+  Query: () => ({
+    user: (root, args) => {
+      return {
+        id: args.id,
+        username: 'coolguy0711',
+        email: 'coolguy0711@live.com',
+        polls: [
+          'Favorite Colors',
+          'Whats your favorite food?',
+          'Favorite framework?',
+        ],
+      };
+    },
+    poll: (root, args) => {
+      return {
+        id: args.id,
+        name: casual.title,
+        options: casual.array_of_words((n = 7)),
+        votes: casual.integer((from = 0), (to = 1000)),
+      };
+    },
+    allPolls: () =>
+      new MockList(5, () => ({
+        id: casual.id,
+        name: casual.title,
+        options: casual.array_of_words((n = 7)),
+        votes: casual.integer((from = 0), (to = 1000)),
+      })),
+  }),
+  User: () => ({
+    id: () => casual.id,
+    username: () => casual.username,
+    email: () => casual.email,
+  }),
+  Poll: () => ({
+    id: () => casual.id,
+    name: () => casual.title,
+    options: () => casual.array_of_words((n = 7)),
+    votes: casual.integer((from = 0), (to = 10000)),
+  }),
+  VoteOption: () => ({
+    option: () => casual.title,
+  }),
 };
--- server/data/schema.js
+++ server/data/schema.js
@@ -6,7 +6,30 @@ const mocks = require('./mocks');
 
 const typeDefs = `
 type Query {
-  testString: String
+  user(id: Int!): User
+  poll(id: Int!): Poll
+  allPolls: [Poll]
+}
+
+type User {
+  id: Int
+  username: String
+  email: String
+  polls: [Poll]
+}
+
+type Poll {
+  id: Int
+  name: String
+  options: [VoteOption]
+  votes: Int
+}
+
+type VoteOption {
+  id: Int
+  name: String
+  votes: Int
+  voters: [User]
 }
 `;
 
